0104662eec41864315c8f3961f5de3d517d0a5a3
oscasais <oscasais@vivaldi.net>
2020-05-16 13:05:49 +0200

    mocha test && travis

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert;
+const lib = require('../dist/pass-hash-generator.cjs.js');
+
+describe('lib', function() {
+    it('sha1 for test:amazon should return Kaqk1bRiiZ55fBYfHTfBd2AD1Hg', function() {
+        assert.equal(lib('test:amazon', 'sha1'),
+            'Kaqk1bRiiZ55fBYfHTfBd2AD1Hg')
+    })
+    it('sha256 for test:amazon should return roZo5udEDdmrFoskl1C61kbm0ocIZOabW6nv3eK42PU', function() {
+        assert.equal(lib('test:amazon', 'sha256'),
+            'roZo5udEDdmrFoskl1C61kbm0ocIZOabW6nv3eK42PU')
+    })
+})
